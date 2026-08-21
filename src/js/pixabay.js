074e5d7935c8f4eada9cc9c73abb48c67b9faa32
AnnVynnychenkoa <avinnichenko93@gmail.com>
2026-08-21 13:46:01 +0200

    refactor: secure API key using process.env

--- src/js/pixabay.js
+++ src/js/pixabay.js
@@ -1,57 +1,54 @@
-import axios from "axios";
+import axios from 'axios';
 
 export class Pixabay {
-    #API_KEY = '37391031-3c842063259c869251b7769d0';
-    #BASE_URL = 'https://pixabay.com/api/';
+  #API_KEY = process.env.PARCEL_PIXABAY_API_KEY;
+  #BASE_URL = 'https://pixabay.com/api/';
 
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
-        this.per_page = 40;
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.per_page = 40;
+  }
+
+  async getPopularPhotos() {
+    try {
+      const response = await axios.get(
+        `${this.#BASE_URL}?key=${
+          this.#API_KEY
+        }&q=popular&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${this.per_page}`
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    async getPopularPhotos() {
-        try {
-          const response = await axios.get(`${
-                    this.#BASE_URL
-                }?key=${
-                    this.#API_KEY
-                }&q=popular&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${this.per_page}`);
-          return response.data;
-        } catch (error) {
-          console.error(error);
-        }
-      }
-
-    async getPhotos() {
-        try {
-          const response = await axios.get(`${
-                    this.#BASE_URL
-                }?key=${
-                    this.#API_KEY
-                }&q=${
-                    this.searchQuery
-                }&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${this.per_page}`);
-          return response.data;
-        } catch (error) {
-          console.error(error);
-        }
-      }
-
-    incrementPage() {
-        this.page +=1;
+  async getPhotos() {
+    try {
+      const response = await axios.get(
+        `${this.#BASE_URL}?key=${this.#API_KEY}&q=${
+          this.searchQuery
+        }&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${this.per_page}`
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    resetPage() {
-        this.page = 1;
-    }
+  incrementPage() {
+    this.page += 1;
+  }
 
-    get query() {
-        return this.searchQuery;
-    }
+  resetPage() {
+    this.page = 1;
+  }
 
-    set query(newQuery) {
-        this.searchQuery = newQuery;
-    }
+  get query() {
+    return this.searchQuery;
+  }
 
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
